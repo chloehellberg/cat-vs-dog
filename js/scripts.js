9d46da0e76fe5af9ffe1ff9ddffe5d8bac9ca186
David Cole <davidtcole@yahoo.com>
2020-08-11 14:24:50 -0700

    create the cat-vs-dog practice project and includes all files and folders.
    Co-authored by: Chloe Hellberg <chloe.hellberg@gmail.com>
    Co-authored by: Amanda Otero <mandystar018@gmail.com>'

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-event.preventDefault();
-})
+  $("#catButton").click(function(){
+    $("#catContainer").append("<p>MEOW!</p>")
+    $("#dogContainer").append("<p>BARK BACK!</p>")
+    $("#catContainer").children("p").last().click(function(){
+      $(this).remove();
+    });
+    $("#dogContainer").children("p").last().click(function(){
+      $(this).remove();
+    });
+  });
+
+
+  $("#dogButton").click(function(){
+    $("#dogContainer").append("<p>WOOF!</p>")
+    $("#catContainer").append("<p>HISS BACK!</p>")
+    $("#dogContainer").children("p").last().click(function(){
+      $(this).remove();
+    });
+    $("#catContainer").children("p").last().click(function(){
+      $(this).remove();
+    });
+  });
+});
